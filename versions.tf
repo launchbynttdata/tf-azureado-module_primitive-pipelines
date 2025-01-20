d20084a6d222a3cbd21aa5d385752e89e5f1b0a9
yogesh-shelke-nttd <yogeshharichandra.shelke@nttdata.com>
2025-01-20 19:45:39 +0530

    updated az provider to 4

--- versions.tf
+++ versions.tf
@@ -16,11 +16,11 @@ terraform {
   required_providers {
     azuredevops = {
       source  = "microsoft/azuredevops"
-      version = "~> 0.11.0"
+      version = "~> 1.0.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.96.0"
+      version = "~> 4.0.0"
     }
   }
 }
